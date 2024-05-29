dc18585591a474d34e231600ac7077a5bc477b63
Satyam020905 <satyamkr922@gmail.com>
2024-05-29 11:47:53 +0530

    small changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -54,7 +54,7 @@ export default function Home() {
                     <h1>Satyam</h1>
                     <p>React Developer | Wordpress Developer | Digital Marketer</p>
                     <div className='icons'>
-                        <a href='https://github.com/Satyam020905'><FaGithub style={{ paddingRight: '1rem',color:'black' }} /></a>
+                        <a href='https://github.com/Satyam020905'><FaGithub style={{ paddingRight: '1rem' }} /></a>
                         <a href=''><FaInstagram style={{ paddingRight: '1rem' }} /></a>
                         <a href=''><FaLinkedin style={{ paddingRight: '1rem' }} /></a>
                     </div>
