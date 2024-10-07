cb8ee63a8a8c0ab75b09cd8f0f21792279f70a8d
Satyam020905 <satyamkr922@gmail.com>
2024-10-07 17:10:56 +0530

    changes

--- src/weather/weather.jsx
+++ src/weather/weather.jsx
@@ -19,7 +19,7 @@ const Weather = () => {
 
     const search = async () => {
         const element = document.getElementsByClassName("cityInput")
-        if (element[0].value === "") {
+        if (element[0]  .value === "") {
             return 0;
         }
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
