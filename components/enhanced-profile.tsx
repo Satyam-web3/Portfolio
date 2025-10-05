881ca80dfcd31a48188bb02b5a4130baee34f0fe
Satyam020905 <satyamkr922@gmail.com>
2025-10-05 12:59:09 +0530

    Availability

--- components/enhanced-profile.tsx
+++ components/enhanced-profile.tsx
@@ -47,7 +47,7 @@ export function EnhancedProfile() {
             ))}
           </div>
           <SocialLinks socialLinks={personalInfo.social} />
-          <div className="p-3 sm:p-4 border-t border-zinc-800 flex items-center justify-center">
+          <div className="p-3 sm:p-4 border-t  flex items-center justify-center">
             <div className="flex items-center">
               <span
                 className={`w-2 h-2 ${personalInfo.availableForWork ? "bg-green-500" : "bg-red-500"} rounded-full mr-2`}
